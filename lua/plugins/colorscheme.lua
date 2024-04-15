27fc0bbe3bd3abe63afdd111dbfa57e52ee94fb3
daschdy <dustin.heither@gmail.com>
2024-04-15 14:55:27 +0200

    no transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -28,7 +28,7 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.g.moonflyTransparent = true
+			-- vim.g.moonflyTransparent = true
 			vim.cmd.colorscheme("moonfly")
 		end,
 	},
