65047e7cc87d7a62504469449137a5dad1e3fb9e
daschdy <dustin.heither@gmail.com>
2024-04-28 19:29:40 +0200

    colorscheme set

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,12 @@
 return {
 	{
-		-- "bluz71/vim-moonfly-colors",
-		-- name = "moonfly",
-		-- lazy = false,
-		-- priority = 1000,
-		-- config = function()
-		-- 	-- vim.g.moonflyTransparent = true
-		-- 	vim.cmd.colorscheme("moonfly")
-		-- end,
-	},
-	{
-		"olivercederborg/poimandres.nvim",
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require("poimandres").setup({
-				-- leave this setup function empty for default config
-				-- or refer to the configuration section
-				-- for configuration options
-			})
-		end,
-
-		-- optionally set the colorscheme within lazy config
-		init = function()
-			vim.cmd("colorscheme poimandres")
+			-- vim.g.moonflyTransparent = true
+			vim.cmd.colorscheme("moonfly")
 		end,
 	},
 }
