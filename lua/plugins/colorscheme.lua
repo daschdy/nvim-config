cb21ac9da5c0947c1f533d115ed7eee2a5da75e2
daschdy <dustin.heither@gmail.com>
2024-05-03 08:17:05 +0200

    rose-pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,26 @@
 return {
 	{
-		"bluz71/vim-moonfly-colors",
-		name = "moonfly",
+		-- "bluz71/vim-moonfly-colors",
+		-- name = "moonfly",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	-- vim.g.moonflyTransparent = true
+		-- 	vim.cmd.colorscheme("moonfly")
+		-- end,
+	},
+	{
+		"rose-pine/neovim",
+		as = "rose-pine",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			-- vim.g.moonflyTransparent = true
-			vim.cmd.colorscheme("moonfly")
+			local rp = require("rose-pine")
+			rp.setup({
+				transparent = true, -- Enable this to disable setting the background color
+				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+			})
+			vim.cmd.colorscheme("rose-pine")
 		end,
 	},
 }
