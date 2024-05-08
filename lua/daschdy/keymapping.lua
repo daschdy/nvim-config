3bd6129d096a9b503ad7404984d5dfc4727c08ed
daschdy <dustin.heither@gmail.com>
2024-05-08 12:18:00 +0200

    keymapping of harpoon centraly stored in keymappings

--- lua/daschdy/keymapping.lua
+++ lua/daschdy/keymapping.lua
@@ -48,3 +48,16 @@ vim.keymap.set("n", "<leader>hs", ":split<CR>", {})
 
 -- Undotree
 vim.keymap.set("n", "<leader>ut", ":UndotreeToggle<CR>", {})
+
+-- Harpoon
+local harpoon = require("harpoon")
+harpoon.setup()
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
