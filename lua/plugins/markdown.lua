6dee88729e7ecca9bc91545fe2969366e0d9ec7b
daschdy <dustin.heither@gmail.com>
2024-05-18 19:12:01 +0200

    minor changes

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"iamcco/markdown-preview.nvim",
+		event = { "VeryLazy"},
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
 		build = function()
