5892011a6b3f1c6759c1ab793ad991249a63d6ca
daschdy <dustin.heither@gmail.com>
2024-06-12 11:44:47 +0200

    changed to folke

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,25 +10,33 @@ return {
 		-- end,
 	},
 	{
-		"navarasu/onedark.nvim",
+		-- "navarasu/onedark.nvim",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	local onedark = require("onedark")
+		-- 	onedark.setup({
+		-- 		theme = "onedark", -- or "onelight"
+		-- 		styles = {
+		-- 			comments = "italic",
+		-- 			keywords = "italic",
+		-- 			functions = "italic",
+		-- 			variables = "italic",
+		-- 		},
+		-- 		transparent = false,
+		-- 		hideInactiveStatusline = true,
+		-- 		invertSignsSelection = true,
+		-- 		invertNormal = false,
+		-- 	})
+		-- 	vim.cmd.colorscheme("onedark")
+		-- end,
+	},
+	{
+		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			local onedark = require("onedark")
-			onedark.setup({
-				theme = "onedark", -- or "onelight"
-				styles = {
-					comments = "italic",
-					keywords = "italic",
-					functions = "italic",
-					variables = "italic",
-				},
-				transparent = false,
-				hideInactiveStatusline = true,
-				invertSignsSelection = true,
-				invertNormal = false,
-			})
-			vim.cmd.colorscheme("onedark")
+			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
 }
